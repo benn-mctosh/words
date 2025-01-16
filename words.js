faa375cc368285cbfd6d71d866f83177c2aa486e
benn-mctosh <107890667+benn-mctosh@users.noreply.github.com>
2025-01-16 10:59:32 -0600

    Add copy to clipboard button

--- words.js
+++ words.js
@@ -65,6 +65,22 @@
   
   **********************/
   
+  function copyText() {
+  console.log("copying")
+  // Get the text field
+  var copyText = document.getElementById("emailText");
+
+  // Select the text field
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(document.getElementById("emailText").innerText);
+
+  // Alert the copied text
+  // alert("Copied the text: " + document.getElementById("emailText").innerText);
+}
+  
   // ok, we need to write our own sample function?
   function sample(arr, N) {
     drawn = []
